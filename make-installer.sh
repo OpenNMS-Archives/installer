01f21e604421b8bf15b1a91e0ac6b633f662ad06
Benjamin Reed <rangerrick@users.sourceforge.net>
2007-10-26 15:53:10 +0000

    start making things more generic

--- make-installer.sh
+++ make-installer.sh
@@ -1,11 +1,29 @@
-#!/bin/sh
+#!/bin/sh -e
 
-IZPACK_COMPILE="/Users/ranger/Local Applications/IzPack/bin/compile"
-BINARY_DIRECTORY="/Users/ranger/source.build/opennms-trunk/target/opennms-1.3.8-SNAPSHOT"
-TEMP_DIRECTORY="/tmp/izpack-temp"
+TOPDIR=`pwd -P`
+IZPACK_HOME="$TOPDIR/izpack"
+IZPACK_COMPILE="$IZPACK_HOME/bin/compile"
+REPLACEMENT_TOKEN="XXX_TOKENIZE_ME_XXX"
+
+export IZPACK_HOME
+
+# build OpenNMS
+if [ -d "$TOPDIR/opennms-build" ]; then
+	svn up "$TOPDIR/opennms-build"
+else
+	svn co http://opennms.svn.sourceforge.net/svnroot/opennms/opennms/trunk "$TOPDIR/opennms-build"
+fi
+
+pushd "$TOPDIR/opennms-build"
+	[ -z "$SKIP_CLEAN" ] && ./build.sh clean
+	./build.sh -Droot.dir="$REPLACEMENT_TOKEN" install assembly:directory-inline
+popd
+
+BINARY_DIRECTORY="$TOPDIR/opennms-build/target/"opennms-*-SNAPSHOT
+TEMP_DIRECTORY="$TOPDIR/izpack-temp"
 
 rsync -avr --delete "$BINARY_DIRECTORY"/ "$TEMP_DIRECTORY"/
-./handle-tokens.pl "$TEMP_DIRECTORY" "$BINARY_DIRECTORY" '$INSTALL_PATH'
+./handle-tokens.pl "$TEMP_DIRECTORY" "$REPLACEMENT_TOKEN" '$INSTALL_PATH'
 
 cp LICENSE UserInputSpec.xml "$TEMP_DIRECTORY/"
 cp discovery-configuration.xml "$TEMP_DIRECTORY/etc/"
