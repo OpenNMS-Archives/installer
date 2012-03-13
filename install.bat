26537f757cedc6f286190c90ae9907865f2580a6
Benjamin Reed <ranger@opennms.org>
2012-03-13 14:56:44 -0400

    bump the default to 512

--- install.bat
+++ install.bat
@@ -4,4 +4,4 @@ SET DBURL=jdbc:postgresql://$izpackDatabaseHost:5432/
 SET ETCDIR=%OPENNMS_HOME%/etc
 SET SERVLETDIR=%OPENNMS_HOME%/webapps/opennms
 
-"$JDKPath\bin\java" -Xmx256m -Dopennms.home=%OPENNMS_HOME% -Dinstall.dir=%OPENNMS_HOME% -Dinstall.database.name=%DBNAME% -Dinstall.database.url=%DBURL% -Dinstall.etc.dir=%ETCDIR% -Dinstall.servlet.dir=%SERVLETDIR% -jar %OPENNMS_HOME%/lib/opennms_install.jar %*
+"$JDKPath\bin\java" -Xmx512m -Dopennms.home=%OPENNMS_HOME% -Dinstall.dir=%OPENNMS_HOME% -Dinstall.database.name=%DBNAME% -Dinstall.database.url=%DBURL% -Dinstall.etc.dir=%ETCDIR% -Dinstall.servlet.dir=%SERVLETDIR% -jar %OPENNMS_HOME%/lib/opennms_install.jar %*
