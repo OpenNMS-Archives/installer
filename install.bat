3af5f5ac9029235cc0114d82ec1a6f8d9ebc757c
Benjamin Reed <rangerrick@befunk.com>
2012-05-08 12:12:09 -0400

    fix executefile

--- install.bat
+++ install.bat
@@ -4,4 +4,4 @@ SET DBURL=jdbc:postgresql://$izpackDatabaseHost:5432/
 SET ETCDIR=%OPENNMS_HOME%/etc
 SET SERVLETDIR=%OPENNMS_HOME%/webapps/opennms
 
-"$JDKPath\bin\java" -Xmx512m "-Dopennms.home=%OPENNMS_HOME%" "-Dinstall.dir=%OPENNMS_HOME%" "-Dinstall.database.name=%DBNAME%" "-Dinstall.database.url=%DBURL%" "-Dinstall.etc.dir=%ETCDIR%" "-Dinstall.servlet.dir=%SERVLETDIR%" -cp "%OPENNMS_HOME%/lib/opennms_bootstrap.jar;%OPENNMS_HOME%/lib/opennms_install.jar" org.opennms.install.Installer %*
+"$JDKPath\bin\java" -Xmx512m "-Dopennms.home=%OPENNMS_HOME%" "-Dinstall.dir=%OPENNMS_HOME%" "-Dinstall.database.name=%DBNAME%" "-Dinstall.database.url=%DBURL%" "-Dinstall.etc.dir=%ETCDIR%" "-Dinstall.servlet.dir=%SERVLETDIR%" -classpath "%OPENNMS_HOME%/lib/opennms_bootstrap.jar;%OPENNMS_HOME%/lib/opennms_install.jar" org.opennms.install.Installer %*
