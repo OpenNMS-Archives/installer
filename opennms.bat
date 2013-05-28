97518b58f106942e874631af07c9b7a692accabf
Benjamin Reed <rangerrick@befunk.com>
2013-05-28 14:58:24 -0400

    sync with some fixes from opennms shell version

--- opennms.bat
+++ opennms.bat
@@ -1,1 +1,1 @@
-"$JDKPath\bin\java" -Xmx256m -XX:MaxPermSize=128m -Dopennms.home="$UNIFIED_INSTALL_PATH" -jar "$UNIFIED_INSTALL_PATH/lib/opennms_bootstrap.jar" %*
+"$JDKPath\bin\java" -Xmx512m -XX:MaxPermSize=256m -Dopennms.home="$UNIFIED_INSTALL_PATH" -Djava.endorsed.dirs="$OPENNMS_HOME/lib/endorsed" -jar "$UNIFIED_INSTALL_PATH/lib/opennms_bootstrap.jar" %*
