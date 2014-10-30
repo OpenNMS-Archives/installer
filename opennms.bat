dc4eacdfc915f485cc2132e1f668663beb6196dd
Markus von Rüden <mvr@opennms.com>
2014-10-30 11:09:38 +0100

    NMS-7114: Fix missing quotes for opennms tmp directory system property.

--- opennms.bat
+++ opennms.bat
@@ -4,7 +4,7 @@
     -DisThreadContextMapInheritable=true^
     -Dopennms.home="$UNIFIED_INSTALL_PATH"^
     -Djava.endorsed.dirs="$UNIFIED_INSTALL_PATH/lib/endorsed"^
-    -Djava.io.tmpdir=$UNIFIED_INSTALL_PATH/data/tmp^
+    -Djava.io.tmpdir="$UNIFIED_INSTALL_PATH/data/tmp"^
     -Dcom.sun.management.jmxremote.port=18980^
     -Dcom.sun.management.jmxremote.ssl=false^
     -Dcom.sun.management.jmxremote.authenticate=false^
